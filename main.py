0d02abb0794d920a8d0c32e274c0e7edccafc7ad
Dongwoo Kang <dongwoomail@icloud.com>
2023-09-04 21:37:16 +0900

    열거형 지원

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-from typing import Optional
+from enum import Enum
 from fastapi import FastAPI
 import uvicorn
 
 app = FastAPI()
 
+# 열거형 지원(다중상속도 지원)
+class UserLevel(str, Enum):
+    a = "a"
+    b = "b"
+    c = "c"
+
 @app.get("/users/") # trailling slash : 307 오류 발생 주의
-def get_user(limit: Optional[int] = None): # 옵셔널 타입 힌트 : Int 거나 Null일 수도 있다. 입력을 하지 않으면 None 발생 
-    return {"limit": limit}
+def get_user(grade: UserLevel): 
+    return {"grade" : grade} 
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
 
-# http ':8000/users?limit=1'
-# http ':8000/users/?limit=1'
-# # http ':8000/users/?'
+# http ":8000/users/?grade=a"
+# http ":8000/users/?grade=d"
+
+# 기본값을 작성하는 경우
+# # def get_user(grade: UserLevel = "a"): #하드코딩 
+# def get_user(grade: UserLevel = UserLevel.a): # 직접 작성
+# http ":8000/users/"
+# 열거형의 기본값을 적을 경우 하드코딩 하는 것보다 직접 작성하는 게 좋습니다.
