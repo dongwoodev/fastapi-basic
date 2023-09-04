7195d7c2211dfe0046a81f12ef353ddf91c9deaf
Dongwoo Kang <dongwoomail@icloud.com>
2023-09-04 20:40:07 +0900

    `httpie` 설치 및 경로 매개변수, 타입 힌트

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-def hello():
-    return "Hello Fast API!"
+@app.get("/users/{user_id}")
+# @app.get("/users/{user_id:int}") # Flask 방식 : 함수에 대한 매개변수를 찾아볼 수 없다. ex) user_id.{}
+def get_user(user_id: int): # 타입 힌트
+    return {"user_id": user_id}
 
 if __name__ == "__main__":
-    uvicorn.run(app)
-    # uvicorn.run("main:app", reload=True)
+    # uvicorn.run(app)
+    uvicorn.run("main:app", reload=True)
