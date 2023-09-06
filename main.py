fc57dab3cfc30f5fe8d312eb7eb2758dc833fbd4
Dongwoo Kang <dongwoomail@icloud.com>
2023-09-06 11:16:47 +0900

    에러 처리 ) 일부로 에러 발생

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-from tempfile import NamedTemporaryFile
-from typing import IO
-
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, HTTPException, status
 
 app = FastAPI()
 
-
-async def save_file(file: IO):
-    # s3 업로드라고 생각해 봅시다. delete=True(기본값)이면
-    # 현재 함수가 닫히고 파일도 지워집니다.
-    with NamedTemporaryFile("wb", delete=False) as tempfile:
-        tempfile.write(file.read()) # 파일을 읽고 tempfile에 씁니다.
-        return tempfile.name
+users = {
+    1: {"name": "Fast"},
+    2: {"name": "Slow"},
+    3: {"name": "API"},
+}
 
 
-@app.post("/file/store")
-async def store_file(file: UploadFile = File(...)):
-    path = await save_file(file.file) # tempfile을 path 저장
-    return {"filepath": path}
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    # 전달 받은 user_id가 users의 key값에 없으면 에러 발생
+    if user_id not in users.keys():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"<User: {user_id}> is not exists.",
+        )
+    # 존재하면 아래처럼 리턴
+    return users[user_id]
 
 """
-python-multipart==0.0.6
-
-contextmanager를 사용하면 다양하게 파일 처리할 수 있습니다.
-
+일부로 에러 발생시키기
 """
