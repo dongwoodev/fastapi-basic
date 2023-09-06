b61292e0e806adba90227cbf0c57810ed8133a50
Dongwoo Kang <dongwoomail@icloud.com>
2023-09-06 10:52:06 +0900

    정적파일과 Form 이용하기

--- main.py
+++ main.py
@@ -1,36 +1,24 @@
-from typing import Optional, List
-
-from fastapi import FastAPI, Cookie, Header
-import uvicorn
+from fastapi import FastAPI, Form
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/header")
-def get_headers(x_token: str = Header(None, title="토큰")):
-    # 헤더를 받아 헤더 반환
-    return {"X-Token": x_token}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+@app.post("/login")
+def login(username: str = Form(...), password: str = Form(...)):
+    return {'username' : username}
 
 """
-# 쿠키
-http :8000/cookie Cookie:ga=123.123.123
+python-multipart==0.0.6
+aiofiles==23.2.1
 
-from fastapi import FastAPI, Cookie
-import uvicorn
+mount : static(정적 파일)을 임포트할 수 있게 끔 도와줍니다. (가장 앞단에 있는 매개변수는 엔드포인트입니다.)
+Form : fastapi에서 Form을 임포트해서 username을 반환합니다.
 
-app = FastAPI()
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload  
 
-@app.get("/cookie")
-def get_cookie(ga: str = Cookie(None)):
-    return {"ga":ga}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
-
-# 헤더
-X- 접두어는 사용자 정의 헤더라는 것을 의미합니다. 
-반드시 이렇게 할 필요는 없지만, 표준 헤더와 구분짓기 위해 사용합니다. 
-http :8000/header X-Token:some.secret.token
+## 주의
+Form 형식을 사용한 경우에는 Json 형식을 사용할 수 없습니다.
+이유는 미디어 타입이 application/json이 아니라 application.x-www-form-urlencoded 이기 때문입니다. 
+이거는 HTTP 스펙에 관한 문제입니다.
 """
