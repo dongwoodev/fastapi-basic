9811fbfe9db8a98e860d095c3c73767ab760c0b7
Dongwoo Kang <dongwoomail@icloud.com>
2023-09-06 11:28:12 +0900

    에러차리) Http 예외처리

--- main.py
+++ main.py
@@ -1,33 +1,32 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import JSONResponse
+from typing import Any, Optional, Dict
+
+from fastapi import FastAPI, HTTPException
+# from starlette.exceptions import HTTPException 
+# FastApi가 Starlette를 상속받지만 starlette은 헤더를 사용할 수 없습니다.
+
+
+class SomeFastAPIError(HTTPException):
+    def __init__(
+        self,
+        status_code: int,
+        detail: Any = None,
+        headers: Optional[Dict[str, Any]] = None,
+    ) -> None:
+        super().__init__(
+            status_code=status_code, detail=detail, headers=headers
+        )
+
 
 app = FastAPI()
-
-# 에러 클래스 정의
-class SomeError(Exception):
-    def __init__(self, name: str, code: int):
-        self.name = name
-        self.code = code
-
-    def __str__(self):
-        return f"<{self.name}> is occured. code: <{self.code}>"
-
-
-app = FastAPI()
-
-#  에러 핸들러 추가
-@app.exception_handler(SomeError)
-async def some_error_handler(request: Request, e: SomeError):
-    return JSONResponse(
-        content={"message": f"error is {e.name}"}, status_code=e.code
-    )
 
 
 @app.get("/error")
 async def get_error():
-    raise SomeError("Hello", 400)
+    raise SomeFastAPIError(500, "Hello")
+
 """
 사용자 정의 에러
 
-에러 핸들러가 있으면 정확히 어떤 에러인지 보여줄 수 있습니다.
+에러 핸들러가 없이 실행 가능합니다.
+ 이는 starlette의 미들웨어가 HTTPException을 처리하고 있기 때문입니다. (기본 HTTP 에러)
 """
